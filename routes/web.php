5a36a7333c3f565430a2aa0265593c32309725e9
anjun <anjunact@qq.com>
2018-03-24 19:50:12 +0800

    post

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,43 @@ Route::post('/cart','Front@cart');
 //     'middleware' => 'auth',
 //     'uses' => 'Front@checkout'
 // ]);
-Route::get('checkout','Front@checkout');
+Route::get('checkout','Front@checkout'); 
 Route::get('/search/{query}','Front@search');
 
 Route::get('auth/login', 'Front@login');
 Route::post('auth/login', 'Front@authenticate');
 Route::get('auth/logout', 'Front@logout');
 Route::post('/register', 'Front@register');
+
+Route::get('/api/v1/products/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+    if ($id == null) {
+        $products = App\Product::all(array('id', 'name', 'price'));
+    } else {
+        $products = App\Product::find($id, array('id', 'name', 'price'));
+    }
+    return Response::json(array(
+                'error' => false,
+                'products' => $products,
+                'status_code' => 200
+            ));
+}]);
+
+Route::get('/api/v1/categories/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+if ($id == null) {
+    $categories = App\Category::all(array('id', 'name'));
+} else {
+    $categories = App\Category::find($id, array('id', 'name'));
+}
+return Response::json(array(
+            'error' => false,
+            'user' => $categories,
+            'status_code' => 200
+        ));
+}]);
+
+
+
+
 
 Route::get('blade', function () {
     $drinks = array('Vodka','Gin','Brandy');
